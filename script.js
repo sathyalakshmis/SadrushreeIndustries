366b1dfdac7943dc86e7720cdc009884668f138b
sathyalakshmi s <sathyalakshmis30@gmail.com>
2021-11-21 20:25:24 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href='about.html'>About</a></li>" +
+"<li><a href='products.html'>Products</a></li>" +
+"<li><a href='contact.html'>Contact Us</a></li>" +
+"</ul>";
